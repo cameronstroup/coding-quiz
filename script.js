966a77ae658d350a2be1a0b66f7ce5997a751058
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-03-04 16:01:34 -0600

    added booleans

--- script.js
+++ script.js
@@ -19,42 +19,74 @@ function startGame() {
   setTime();
   console.log(questionElement);
 }
-git;
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+    if (secondsLeft < 1) {
+      endGameScreen();
+    }
+  }, 2000);
+}
+
 function nextQuestion() {
   questionElement.innerText = questions[0].question;
   buttons = [buttonA, buttonB, buttonC, buttonD];
 
   for (i = 0; i < questions.length; i++) {
-    buttons[i].innerText = questions[questionCounter].answers[i];
+    buttons[i].innerText = questions[questionCounter].answers[i].text;
   }
+
   questionCounter++;
 }
 
 var questions = [
   {
     question: "Who created Javascript",
-    answers: ["Brendan Eich", "Steve Jobs", "Cameron Stroup", "Bill Gates"],
+    answers: [
+      { text: "Brendan Eich", correct: true },
+      { text: "Steve Jobs", correct: false },
+      { text: "Cameron Stroup", correct: false },
+      { text: "Bill Gates", correct: false },
+    ],
   },
   {
     question: 'Javascript is the "   " of a webstite? ',
     answers: [
-      "Complete Webpage Interface",
-      "Content and Structure",
-      "Design and Display",
-      "Backend",
+      { text: "Complete Webpage Interface", correct: true },
+      { text: "Content and Structure", correct: false },
+      { text: "Design and Display", correct: false },
+      { text: "Backend", correct: false },
     ],
   },
   {
     question: "Which is not a daya types supported by JavaScript?",
-    answers: ["Boolean", "String", "Object", "Fucntion"],
+    answers: [
+      { text: "Boolean", correct: false },
+      { text: "String", correct: false },
+      { text: "Object", correct: false },
+      { text: "Fucntion", correct: true },
+    ],
   },
   {
     question: "Which is not a feature of Javascirpt?",
     answers: [
-      "It is a lightweight, interpreted programming language.",
-      "It is designed for creating network-centric applications.",
-      "It is complementary to and integrated with Java",
-      "It is extremly easy to understand and does not need much practice ",
+      {
+        text: "It is a lightweight, interpreted programming language.",
+        correct: false,
+      },
+      {
+        text: "It is designed for creating network-centric applications.",
+        correct: false,
+      },
+      {
+        text: "It is complementary to and integrated with Java",
+        correct: false,
+      },
+      {
+        text: "It is extremly easy to understand and does not need much practice ",
+        correct: true,
+      },
     ],
   },
 ];
